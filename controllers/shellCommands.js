8ccae8bf30cc52e8d80d06d3ab74125e838a0719
TerraSeraph <TerraSeraph@gmail.com>
2019-03-30 05:10:29 +1000

    updates output for shell

--- controllers/shellCommands.js
+++ controllers/shellCommands.js
@@ -1,34 +1,44 @@
 //@ts-check
-var shell = require('shelljs')
-const path = require('path');
-var directory = path.join(__dirname, `../`)
-
+var shell = require("shelljs");
+const path = require("path");
+var directory = path.join(__dirname, `../`);
 
 function restartBranchServer() {
-    shell.exec('sudo reboot',(result)=>{
-        console.log(result);
-        
-    });
-    shell.exit(1);
+  shell.exec("sudo reboot", result => {
+    console.log(result);
+  });
+  shell.exit(1);
 }
-exports.restartBranchServer = restartBranchServer
+exports.restartBranchServer = restartBranchServer;
 
 function reloadBranchDesktop() {
-    shell.exec('sudo service lightdm restart');
-    shell.exit(1);
+  var cmd = ("sudo service lightdm restart");
+      shell.exec(`${cmd}`, function(code, stdout, stderr) {
+    console.log('Exit code:', code);
+    console.log('Program output:', stdout);
+    console.log('Program stderr:', stderr);
+});
+  shell.exit(1);
 }
-exports.reloadBranchDesktop = reloadBranchDesktop
+exports.reloadBranchDesktop = reloadBranchDesktop;
 
 function customCommand(cmd) {
-    shell.exec(`${cmd}`);
-    shell.exit(1);
+  shell.exec(`${cmd}`, function(code, stdout, stderr) {
+    console.log("Exit code:", code);
+    console.log("Program output:", stdout);
+    console.log("Program stderr:", stderr);
+  });
+  shell.exit(1);
 }
-exports.customCommand = customCommand
-
+exports.customCommand = customCommand;
 
 function gitUpdate() {
-    let cmd = `bash -c 'cd ${directory} && git pull && npm install && sudo reboot'`
-    shell.exec(`${cmd}`);
-    shell.exit(1);
+  let cmd = `bash -c 'cd ${directory} && git pull && npm install && sudo reboot'`;
+  shell.exec(`${cmd}`, function(code, stdout, stderr) {
+    console.log("Exit code:", code);
+    console.log("Program output:", stdout);
+    console.log("Program stderr:", stderr);
+  });
+  shell.exit(1);
 }
-exports.gitUpdate = gitUpdate
+exports.gitUpdate = gitUpdate;
