531b6b78ffdc1b0428b9bb69b351fc30e7580f3d
TerraSeraph <TerraSeraph@gmail.com>
2019-03-30 06:00:26 +1000

    updated shell, added forever

--- controllers/shellCommands.js
+++ controllers/shellCommands.js
@@ -4,41 +4,57 @@ const path = require("path");
 var directory = path.join(__dirname, `../`);
 
 function restartBranchServer() {
-  shell.exec("sudo reboot", result => {
-    console.log(result);
-  });
-  shell.exit(1);
+    var cmd = 'sudo reboot'
+        var bash = shell.exec(`${cmd}`, {
+        async: true
+    })
+    // @ts-ignore
+    bash.stdout.on('data', function (data) {
+        console.log(data)
+        // @ts-ignore
+        bash.kill();
+    });
 }
 exports.restartBranchServer = restartBranchServer;
 
 function reloadBranchDesktop() {
-  var cmd = ("sudo service lightdm restart");
-      shell.exec(`${cmd}`, function(code, stdout, stderr) {
-    console.log('Exit code:', code);
-    console.log('Program output:', stdout);
-    console.log('Program stderr:', stderr);
-});
-  shell.exit(1);
+    var cmd = ("sudo service lightdm restart");
+    var bash = shell.exec(`${cmd}`, {
+        async: true
+    })
+    // @ts-ignore
+    bash.stdout.on('data', function (data) {
+        console.log(data)
+        // @ts-ignore
+        bash.kill();
+    });
 }
 exports.reloadBranchDesktop = reloadBranchDesktop;
 
 function customCommand(cmd) {
-  shell.exec(`${cmd}`, function(code, stdout, stderr) {
-    console.log("Exit code:", code);
-    console.log("Program output:", stdout);
-    console.log("Program stderr:", stderr);
-  });
-  shell.exit(1);
+    var bash = shell.exec(`${cmd}`, {
+        async: true
+    })
+    // @ts-ignore
+    bash.stdout.on('data', function (data) {
+        console.log(data)
+        // @ts-ignore
+        bash.kill();
+    });
+
 }
 exports.customCommand = customCommand;
 
 function gitUpdate() {
-  let cmd = `bash -c 'cd ${directory} && git pull && npm install && sudo reboot'`;
-  shell.exec(`${cmd}`, function(code, stdout, stderr) {
-    console.log("Exit code:", code);
-    console.log("Program output:", stdout);
-    console.log("Program stderr:", stderr);
-  });
-  shell.exit(1);
+    let cmd = `bash -c 'cd ${directory} && git pull && npm install && sudo reboot'`;
+    var bash = shell.exec(`${cmd}`, {
+        async: true
+    })
+    // @ts-ignore
+    bash.stdout.on('data', function (data) {
+        console.log(data)
+        // @ts-ignore
+        bash.kill();
+    });
 }
 exports.gitUpdate = gitUpdate;
